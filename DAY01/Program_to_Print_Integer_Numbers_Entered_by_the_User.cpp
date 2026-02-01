9f7d51e2a49e4d39d3b200bab40f4cbf548fdefe
Amuktha Goteti <amukthagoteti55@gmail.com>
2026-02-01 13:33:20 +0530

    add DAY01/Program_to_Print_Integer_Numbers_Entered_by_the_User.cpp

---
+++ DAY01/Program_to_Print_Integer_Numbers_Entered_by_the_User.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "Enter an integer number: ";
+    cin >> number;
+    cout << "You entered: " << number << endl;
+    return 0;
+}
